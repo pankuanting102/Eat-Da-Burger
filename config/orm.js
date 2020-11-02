fb4eea9421dc33d9e22e537f360ca1f14c85df62
Kuanting Pan <pankuanting@gmail.com>
2020-11-02 00:18:02 -0500

    test get all

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,43 @@
 var connection = require("../config/connection.js");
 
-var orm = {
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+  
+  function objToSql(ob) {
+    var arr = [];
+  
+    for (var key in ob) {
+      var value = ob[key];
+ 
+      if (Object.hasOwnProperty.call(ob, key)) {
 
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        arr.push(key + "=" + value);
+      }
+    }
+
+    return arr.toString();
+  }
+
+var orm = {
+all: function(tableInput, cb) {
+    var queryString = " SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result){
+        if (err){
+            throw err;
+        }
+        cb(result);
+    });
+},
 
 
 }
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,11 +1,20 @@
 var express = require("express");
 
 var router = express.Router();
-var burgers = require("../models/cat.js")
+var burgers = require("../models/burger.js");
+const burger = require("../../cu-nyc-fsf-pt-07-2020-u-c/13-MVC/02-Homework/Main/BurgerSolution/models/burger.js");
 
-router.get()
+router.get("/", function(req, res){
+    res.redirect("/burgers")
+})
+
+router.get("/burgers", function (req, res){
+    burger.all(function(burgerData){
+        res.render("index", {burger_data: burgerData})
+    })
+})
 
 router.post()
 
-router.delete()
+router.put()
 module.exports = router;
