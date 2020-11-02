2833f6fa16ca1eef8d439d4e5b5d5bf229a7ba2c
Kuanting Pan <pankuanting@gmail.com>
2020-11-01 19:59:21 -0500

    set up structure

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,11 @@
+var express = require("express");
+
+var router = express.Router();
+var burgers = require("../models/cat.js")
+
+router.get()
+
+router.post()
+
+router.delete()
+module.exports = router;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,8 @@
-USE bugers_db
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheese Burger", false);
 
 INSERT INTO burgers (burger_name, devoured)
-VALUES ("Cheese Burger", true);
-
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("Chicken Sandwich", true);
+VALUES ("Chicken Sandwich", false);
 
 INSERT INTO burgers (burger_name, devoured)
 VALUES ("Susage Eggs and Cheese", false);
-
-SELECT * FROM burgers;
